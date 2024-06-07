e1d0db8e8ecb43352db6699a35a733f69b0a9464
Paul Green <traktion0@gmail.com>
2024-06-07 16:01:54 +0100

    - Updated to include sn_httpd 0.3.3 and updated beta network contacts/peers.

--- sn_httpd/sn_httpd_wrapper.sh
+++ sn_httpd/sn_httpd_wrapper.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 if [ -z $1 ]; then
-  export NETWORK_CONTACT=`curl -sSL https://sn-testnet.s3.eu-west-2.amazonaws.com/alpha-punch-network-contacts | head -1`;
+  export NETWORK_CONTACT=`curl -sSL https://sn-testnet.s3.eu-west-2.amazonaws.com/network-contacts | head -1`;
 else
   export NETWORK_CONTACT=$1;
 fi
 
 if [ -z $2 ]; then
-  export DNS_REGISTER="6d70bf50aec7ebb0f1b9ff5a98e2be2f9deb2017515a28d6aea0c6f80a9f44ddaf480334bbff0cd23302b386bfc071a51d132e8e13055701a94211eaada0b6b4c37a94f8adc1e96be1aa1f149b7faffa";
+  export DNS_REGISTER="6d70bf50aec7ebb0f1b9ff5a98e2be2f9deb2017515a28d6aea0c6f80a9f44dd8128aad4427271f22b4f1444119b3243adbd5c48ca15edd7ab3dd56e4017a82126a4d2b5fe49d53fcfac7beef03f748e";
 else
   export DNS_REGISTER=$2;
 fi
 
-export SAFE_PEERS=/ip4/142.93.46.42/udp/43338/quic-v1/p2p/12D3KooWSaCWsF2qLULtKTtjJPYpzCKn3X3aY19WdznPB5jUfmt3
-safe wallet get-faucet 209.97.185.196:8000
+export SAFE_PEERS=/ip4/178.128.34.57/udp/42231/quic-v1/p2p/12D3KooWSn9FhKHBbTRuS9FfydrfQ17tGGDByW6SJiSMeW3Gogdo
+#safe wallet get-faucet 209.97.185.196:8000
 
 sn_httpd 0.0.0.0:8080 static $NETWORK_CONTACT $DNS_REGISTER
